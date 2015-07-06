f6ebd0281596160ea7dfa9704c7bdc2af29f82ff
Mithgol the Webmaster <getgit@mithgol.ru>
2015-07-06 22:41:40 +0300

    configure Autumn exports, expecting format `AutumnUser <name> <path>`

--- gyConfig.js
+++ gyConfig.js
@@ -6,5 +6,19 @@ module.exports = function(configOptions){
       skipNames: ['//', '#']
    });
 
+   var autumnUserDefs = configGamayun.all('AutumnUser'); // or `null`
+   if( autumnUserDefs === null ) autumnUserDefs = [];
+   setup.autumnUsers = autumnUserDefs.map(function(nextAutumnUserDef){
+      var matches = /^\s*(\S+)\s+(\S(?:.*\S)?)\s*$/.exec(nextAutumnUserDef);
+      if( matches === null ) return null;
+
+      return {
+         name: matches[1],
+         path: matches[2]
+      };
+   }).filter(function(nextAutumnUserDef){
+      return nextAutumnUserDef !== null;
+   });
+
    return setup;
 };
